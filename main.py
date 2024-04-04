8f5f7f0ed8cd32216090ddda42670d524b8aa1fc
andycr00 <andres.cr00@hotmail.com>
2024-04-04 10:29:08 -0500

    ..

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pickle
 from flask import Flask, request
-import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def hello_world():
-    clf_xgb = pickle.load(open('model.pkl', 'rb'))
+    clf_xgb = pickle.load(open("model.pkl", "rb"))
     vals = request.get_json().values()
-    pred = clf_xgb.predict([list(vals)])
     proba = clf_xgb.predict_proba([list(vals)])
-    
-    return {'mortalidad': int(pred), 'probabilidad': float(proba[:, 1])}
+
+    return {
+        "mortalidad": "Alto Riesgo" if float(proba[:, 1]) > 0.6 else "Bajo Riesgo",
+        "probabilidad": float(proba[:, 1]),
+    }
