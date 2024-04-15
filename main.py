72ed5702267b337cff9c4db3f47e939ff40d157b
andycr00 <andres.cr00@hotmail.com>
2024-04-15 13:25:05 -0500

    ...

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
 import pickle
-from flask import Flask, request
+from flask import Flask, Response, request
+import json
+from manager import DAO
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST"])
 def hello_world():
-    clf_xgb = pickle.load(open("model.pkl", "rb"))
-    vals = request.get_json().values()
-    proba = clf_xgb.predict_proba([list(vals)])
+    try:
+        clf_xgb = pickle.load(open("model.pkl", "rb"))
+        json_req = request.get_json()
 
-    return {
-        "mortalidad": "Alto Riesgo" if float(proba[:, 1]) > 0.6 else "Bajo Riesgo",
-        "probabilidad": float(proba[:, 1]),
-    }
+        dict = json.load(open("model_dict.json", "r"))
+
+        proba = predict(json_req["attr"], dict["mortalidad"], clf_xgb)
+
+        dao = DAO()
+
+        res = {
+            "message": "Prediction calculated successfully",
+            "mortality": "Alto Riesgo" if proba > 0.6 else "Bajo Riesgo",
+            "probability": f"{proba * 100:.2f} %",
+        }
+
+        for i in res:
+            json_req[i] = res[i]
+
+        dao.insertCalculation(json_req)
+
+        return json_response(res, 200)
+    except KeyError:
+        missing = [
+            validate_params(json_req, dict["min_attributes"]),
+            validate_params(json_req["attr"], dict["mortalidad"]),
+        ]
+        return json_response(
+            {
+                "message": "Incomplete params",
+                "details": "Request body does not fit the requirements",
+                "params_missing": [x for x in missing if x is not False][0],
+            },
+            409,
+        )
+
+
+def predict(params, dict, model):
+    vals = [params[key] for key in dict]
+    return float(model.predict_proba([vals])[:, 1])
+
+
+def validate_params(json_obj, dict):
+    values = [i for i in dict if i not in list(json_obj.keys())]
+    return False if len(values) == 0 else values
+
+
+def json_response(json_obj, status):
+    return Response(json.dumps(json_obj), status=status, mimetype="application/json")
---
+++ manager.py
@@ -0,0 +1,61 @@
+import sqlite3, json, datetime
+from uuid import uuid4
+
+
+class DAO:
+
+    def __init__(self, *args, **kwargs):
+        self.connection = sqlite3.connect("db.sqlite")
+        self.cursor = self.connection.cursor()
+
+    def createTables(self):
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS user (
+                    id TEXT PRIMARY KEY, 
+                    name TEXT, 
+                    email TEXT
+            )
+            """
+        )
+
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS calculations (
+                    id TEXT PRIMARY KEY, 
+                    user_id TEXT,
+                    results TEXT, 
+                    date TEXT,
+                    FOREIGN KEY(user_id) REFERENCES user(id)
+            )
+            """
+        )
+
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS outcome (
+                    id TEXT PRIMARY KEY, 
+                    calc_id TEXT,
+                    outcome TEXT,
+                    FOREIGN KEY(calc_id) REFERENCES calculations(id)
+            )
+            """
+        )
+
+        self.connection.commit()
+
+    def insertCalculation(self, json_obj):
+        user_id = json_obj["user_id"]
+        del json_obj["user_id"]
+        self.cursor.execute(
+            f"""
+            INSERT INTO calculations VALUES(
+                '{uuid4()}', 
+                '{user_id}',
+                '{json.dumps(json_obj)}',
+                '{datetime.datetime.now()}'
+            )
+            """
+        )
+
+        self.connection.commit()
